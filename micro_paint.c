083a05ecd0904dc807603c6dad3ee4c76a23aead
Aloïs Sebrechts <alois@Aloiss-MacBook-Pro.local>
2021-11-26 17:26:08 +0100

    parsing ok

--- micro_paint.c
+++ micro_paint.c
@@ -1,33 +1,110 @@
 #include "micro_paint.h"
 
-int	parsing(t_info *info)
+int	ft_isspace(char c)
 {
-	char	space0;
-	char	space1;
-	char	end;
-
-	fscanf(info->ptr, "%d%c%d%c%c%c", &info->width, &space0, &info->height, &space1, &info->back, &end);
-	printf("%d\n", info->width);
-	printf("%d\n", (int)space0);
-	printf("%d\n", info->height);
-	printf("%d\n", (int)space1);
-	printf("%c\n", info->back);
-	printf("%d\n", (int)end);
+	if (c == '\n' || c == '\v' || c == '\t' || c == '\f' || c == '\r' || c == ' ')
+		return (1);
+	else
+		return (0);
 }
+
+int	check_s(void *ptr)
+{
+	char c;
+
+	fscanf(ptr, "%c", &c);	
+	printf("%c", c);
+	if (!(ft_isspace(c)))
+		return (1);
+	else
+		return (0);
+}
+
+int	first_line(void *ptr)
+{
+	char	c;
+	int	n;
+
+	fscanf(ptr, " %d", &n);
+	if (n > 300 || n <= 0)
+		return (1);	
+	printf("%d", n);
+	if (check_s(ptr))
+		return (1);
+	fscanf(ptr, " %d", &n);
+	if (n > 300 || n <= 0)
+		return (1);
+	printf("%d", n);
+	if (check_s(ptr))
+		return (1);
+	if (fscanf(ptr, " %c", &c) == EOF)
+		return (1);
+	printf("%c\n", c);
+	return (0);
+}
+
+int	parsing(void *ptr)
+{
+	char	c;
+	float	f;
+
+	if (fscanf(ptr, " %c", &c) == EOF)
+		return (0);
+	if (c != 'r' && c != 'R')
+		return (1);
+	printf("%c", c);
+	if (check_s(ptr))
+		return (1);
+	fscanf(ptr, " %f", &f);
+	printf("%f", f);
+	if (check_s(ptr))
+		return (1);
+	fscanf(ptr, " %f", &f);
+	printf("%f", f);
+	if (check_s(ptr))
+		return (1);
+	fscanf(ptr, " %f", &f);
+	if (f <= 0)
+		return (1);
+	printf("%f", f);
+	if (check_s(ptr))
+		return (1);
+	fscanf(ptr, " %f", &f);
+	printf("%f", f);
+	if (f <= 0)
+		return (1);
+	if (check_s(ptr))
+		return (1);
+	if (fscanf(ptr, " %c", &c) == EOF)
+		return (1);
+	printf("%c\n", c);
+	return(parsing(ptr));
+}
+
 
 int	main(int ac, char **av)
 {
-	t_info	info;
+	void	*ptr;
+	int	i;
 
 	if (ac == 2)
 	{
-		if(!(info.ptr = fopen(av[1], "r")))
+		if(!(ptr = fopen(av[1], "r")))
 		{
 			write(1, "Error: Operation file corrupted\n", 32);
 			return(1);
 		}
-		parsing(&info);
-		fclose(info.ptr);
+		if (first_line(ptr))
+		{
+			write(1, "Error: Operation file corrupted\n", 32);
+			return(1);
+		}
+		if (parsing(ptr))
+		{
+			write(1, "Error: Operation file corrupted\n", 32);
+			return(1);
+		}
+		fclose(ptr);
 	}
 	else
 		write(1, "Error: argument\n", 16);
